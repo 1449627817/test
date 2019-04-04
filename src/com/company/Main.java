c8833c3e3008ffda81d5c913236395208e479f39
shm <1449627817@qq.com>
2019-04-04 11:48:28 +0800

    first test

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    private static String say ="我要学会你";
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("你好 Java"+say);
+        int a=1;
+        int b=2;
+        int c=a+++b;
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
